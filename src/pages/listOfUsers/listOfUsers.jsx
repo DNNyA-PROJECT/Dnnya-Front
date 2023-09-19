3d480832eef5246e93551a75b849f79e9f4b2fb1
Leandro joel Ramos <leandrojoelramos7@gmail.com>
2023-09-19 01:19:14 -0300

    mod : list of users

--- src/pages/listOfUsers/listOfUsers.jsx
+++ src/pages/listOfUsers/listOfUsers.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { colors } from '../../assets/styles/theme.js'
 import '../../assets/styles/styles.css'
@@ -6,43 +6,76 @@ import '../../assets/styles/normalize.css'
 import Footer from '../../components/partials/footer.jsx'
 import Menu from '../../components/partials/Menu.jsx'
 import DataTable from '../../components/dataTable/dataTable.jsx';
+import CustomModal from '../../components/modal/modal';
 
 window.themeColors = colors;
 
 function ListOfUsers() {
+    const [modalShow, setModalShow] = useState(false);
+
+    const handleShowModal = () => {
+        setModalShow(true);
+    }
+
+    const handleCloseModal = () => {
+        setModalShow(false);
+    }
+
+    const ButtonWithModal = () => {
+        return (
+            <button className='folderButton' onClick={handleShowModal}>
+                <i class="bi bi-folder-fill"></i>
+            </button>
+        );
+    }
+
     const customData = [
         ["Nombre y Apellido", "Información del Solicitante", "Fecha de Solicitud", "Dar de Alta / Baja"],
-        
-        ["Alejandro Bianchi", "Carpeta", "29/08/23", "boton"],
-        ["Alejandro Bianchi", "Carpeta", "29/08/23", "boton"],
-        ["Alejandro Bianchi", "Carpeta", "29/08/23", "boton"],
-        ["Alejandro Bianchi", "Carpeta", "29/08/23", "boton"],
-        ["Alejandro Bianchi", "Carpeta", "29/08/23", "boton"],
-        ["Alejandro Bianchi", "Carpeta", "29/08/23", "boton"],
-        ["Alejandro Bianchi", "Carpeta", "29/08/23", "boton"],
-        ["Alejandro Bianchi", "Carpeta", "29/08/23", "boton"],
-        ["Alejandro Bianchi", "Carpeta", "29/08/23", "boton"],
-        ["Alejandro Bianchi", "Carpeta", "29/08/23", "boton"],
-        ["Alejandro Bianchi", "Carpeta", "29/08/23", "boton"],
-        ["Alejandro Bianchi", "Carpeta", "29/08/23", "boton"],
-        ["Alejandro Bianchi", "Carpeta", "29/08/23", "boton"],
-        ["Alejandro Bianchi", "Carpeta", "29/08/23", "boton"],
-        ["Alejandro Bianchi", "Carpeta", "29/08/23", "boton"],
-        ["Alejandro Bianchi", "Carpeta", "29/08/23", "boton"],
-      ];
+        ["Alejandro Bianchi", <ButtonWithModal />, "29/08/23", "boton"],
+        ["Alejandro Bianchi", <ButtonWithModal />, "29/08/23", "boton"],
+        ["Alejandro Bianchi", <ButtonWithModal />, "29/08/23", "boton"],
+        ["Alejandro Bianchi", <ButtonWithModal />, "29/08/23", "boton"],
+        ["Alejandro Bianchi", <ButtonWithModal />, "29/08/23", "boton"],
+        ["Alejandro Bianchi", <ButtonWithModal />, "29/08/23", "boton"],
+        ["Alejandro Bianchi", <ButtonWithModal />, "29/08/23", "boton"],
+        ["Alejandro Bianchi", <ButtonWithModal />, "29/08/23", "boton"],
+        ["Alejandro Bianchi", <ButtonWithModal />, "29/08/23", "boton"],
+        ["Alejandro Bianchi", <ButtonWithModal />, "29/08/23", "boton"],
+        ["Alejandro Bianchi", <ButtonWithModal />, "29/08/23", "boton"],
+        ["Alejandro Bianchi", <ButtonWithModal />, "29/08/23", "boton"],
+        ["Alejandro Bianchi", <ButtonWithModal />, "29/08/23", "boton"],
+    ];
 
     return (
         <>
-       <div className='box container-fluid row p-0 ' style={{ backgroundColor: window.themeColors.footerBackground.bakgroundFColor }}>
+            <div className='box container-fluid row p-0 ' style={{ backgroundColor: window.themeColors.footerBackground.bakgroundFColor }}>
                 <div className='col-md-3 container-fluid p-0 menubox' style={{ backgroundColor: window.themeColors.color }}>
                     <Menu />
                 </div>
                 <div className='container-fluid d-flex flex-column m-0 p-0 col-9'>
                     <div>
-                       <h1 className='text-first d-flex justify-content-center'> Listado de Usuarios y Solicitantes </h1>
+                        <h1 className='text-first d-flex justify-content-center'> Listado de Usuarios y Solicitantes </h1>
                     </div>
                     <div className='container-fluid table-container p-0 m-0' style={{ overflowX: 'auto', overflowY: 'auto', maxHeight: '30vw' }}>
-                    <DataTable data={customData} headerBackgroundColor="#FFA500" />
+                        <DataTable data={customData} headerBackgroundColor="#F2A57F" />
+                        <CustomModal
+                            title="Informacion del Usuario"
+                            show={modalShow}
+                            handleClose={handleCloseModal}
+                            body={
+                                <div>
+                                    <h5>.Informacion Personal</h5>
+                                      <p>Nombre Completo: Alejandro Bianchi</p>
+                                      <p>Email</p>
+                                      <p>Usuario</p>
+                                      <p>Foto de Perfil</p>
+                                    <h5>.Rol</h5>
+                                    No Asignado
+                                    <h5>.Permisos</h5>
+                                    No tiene
+                                </div>
+                            }
+                        />
                     </div>
                 </div>
             </div>
@@ -54,3 +87,4 @@ function ListOfUsers() {
 }
 
 export default ListOfUsers;
+
