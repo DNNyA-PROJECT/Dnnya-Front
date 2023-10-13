51aff2094f132fd3f75a0d61aae5877a2819413b
Leandro joel Ramos <leandrojoelramos7@gmail.com>
2023-10-13 15:32:18 -0300

    mod : modal

--- src/components/modal/modal.jsx
+++ src/components/modal/modal.jsx
@@ -2,35 +2,58 @@ import React, { useState } from 'react';
 import Modal from 'react-bootstrap/Modal';
 import Button from 'react-bootstrap/Button';
 
-function CustomModal({ title, body, show, handleClose }) {
+function CustomModal({ title, body, show, handleClose, showNavButtons, onNavButtonClick }) {
   return (
-<Modal show={show} onHide={handleClose}>
-  <div style={{ backgroundColor: window.themeColors.modalBackgroundColor }}>
-    <Modal.Header closeButton>
-      <Modal.Title>{title}</Modal.Title>
-    </Modal.Header>
-    <Modal.Body>{body}</Modal.Body>
-    <Modal.Footer>
-      <Button variant="secondary" onClick={handleClose} className='btn register btn-lg'
-        style={{
-          backgroundColor: window.themeColors.buttonColor,
-          color: window.themeColors.footerColorText
-        }} >
-        Cerrar
-      </Button>
-      <Button
-        onClick={handleClose}
-        className='btn register btn-lg'
-        style={{
-          backgroundColor: window.themeColors.buttonColor,
-          color: window.themeColors.footerColorText
-        }}
-      >
-        Guardar Cambios
-      </Button>
-    </Modal.Footer>
-  </div>
-</Modal>
+    <Modal show={show} onHide={handleClose}>
+      <div style={{ backgroundColor: window.themeColors.modalBackgroundColor }}>
+        <Modal.Header closeButton>
+          <Modal.Title>{title}</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>{body}</Modal.Body>
+        <Modal.Footer>
+          {showNavButtons && (
+            <>
+              <Button
+                variant="primary"
+                onClick={() => onNavButtonClick('previous')}
+                className='btn register btn-lg'
+                style={{
+                  backgroundColor: window.themeColors.buttonColor,
+                  color: window.themeColors.footerColorText
+                }}>
+                <i className="bi bi-arrow-left-square-fill"></i>
+              </Button>
+              <Button
+                variant="primary"
+                onClick={() => onNavButtonClick('next')}
+                className='btn register btn-lg'
+                style={{
+                  backgroundColor: window.themeColors.buttonColor,
+                  color: window.themeColors.footerColorText
+                }}>
+                <i className="bi bi-arrow-right-square-fill"></i>
+              </Button>
+            </>
+          )}
+          <Button variant="secondary" onClick={handleClose} className='btn register btn-lg'
+            style={{
+              backgroundColor: window.themeColors.buttonColor,
+              color: window.themeColors.footerColorText
+            }}>
+            Cerrar
+          </Button>
+          <Button
+            onClick={handleClose}
+            className='btn register btn-lg'
+            style={{
+              backgroundColor: window.themeColors.buttonColor,
+              color: window.themeColors.footerColorText
+            }}>
+            Guardar Cambios
+          </Button>
+        </Modal.Footer>
+      </div>
+    </Modal>
   );
 }
 
--- src/pages/listOfUsers/listOfUsers.jsx
+++ src/pages/listOfUsers/listOfUsers.jsx
@@ -9,21 +9,57 @@ import Button from 'react-bootstrap/Button';
 import Searcher from '../../components/searcher/searcher.jsx'
 
 function ListOfUsers() {
-    const [modalShow, setModalShow] = useState(false);
+    const [showModal, setShowModal] = useState(false);
     const [currentModal, setCurrentModal] = useState(1);
 
-    const handleShowModal = (modalNumber) => {
-        setModalShow(true);
-        setCurrentModal(modalNumber);
+    // Función para manejar el cierre del modal
+    const handleCloseModal = () => {
+        setShowModal(false);
     };
 
-    const handleCloseModal = () => {
-        setModalShow(false);
+    // Función para manejar la navegación entre modales
+    const handleNavButtonClick = (direction) => {
+        if (direction === 'previous') {
+            setCurrentModal(1);
+        } else if (direction === 'next') {
+            setCurrentModal(2);
+        }
     };
+
+    const modalBody = currentModal === 1 ? (
+
+        <div>
+            <h5>Información Personal</h5>
+            <p>Nombre Completo: Alejandro Bianchi</p>
+            <p>Email: alebianchi@gmail.com</p>
+            <p>Usuario: tubebitofiufiu</p>
+            <p>Foto de Perfil</p>
+            <h5>Rol</h5>
+            <select name="role" id="role">
+                <option value="Admin">Administrador</option>
+                <option value="User">Usuario</option>
+            </select>
+            <h5>Permisos</h5>
+            No tiene
+        </div>
+
+    ) : (
+        <div>
+            <h5>Información del contacto</h5>
+            <p>Dirección Actual</p>
+            <p>Número de Teléfono (fijo o celular)</p>
+            <p>Dirección de correo Electrónico</p>
+            <h5>Información de Emergencia</h5>
+            <p>vacio</p>
+            <p>vacio</p>
+            <h5>Información del Historial Educativo</h5>
+            <p>vacio</p>
+        </div>
+    );
 
     const ButtonFolder = () => {
         return (
-            <button className='folderButton' onClick={() => handleShowModal(2)}>
+            <button className='folderButton' onClick={() => setShowModal(true)}>
                 <i className="bi bi-folder-fill"></i>
             </button>
         );
@@ -46,108 +82,29 @@ function ListOfUsers() {
 
     };
 
-    const modalBody = currentModal === 1 ? (
-        <div>
-            <h5>.Informacion del contacto</h5>
-            <p>Dirección Actual</p>
-            <p>Número de Teléfono (fijo o celular)</p>
-            <p>Dirección de correo Electrónico </p>
-            <h5>.Información de Emergencia</h5>
-            <p>vacio</p>
-            <p>vacio</p>
-            <h5>.Informacion del Historial Educativo</h5>
-            <p>vacio</p>
-            <div className="d-flex justify-content-between">
-                <Button
-                    variant="primary"
-                    onClick={() => handleShowModal(2)}
-                    className='btn register btn-lg'
-                    style={{
-                        backgroundColor: window.themeColors.buttonColor,
-                        color: window.themeColors.footerColorText
-                    }}
-                >
-                    <i className="bi bi-arrow-left-square-fill"></i>
-                </Button>
-                <Button
-                    variant="primary"
-                    onClick={handleCloseModal}
-                    className='btn register btn-lg'
-                    style={{
-                        backgroundColor: window.themeColors.buttonColor,
-                        color: window.themeColors.footerColorText
-                    }}
-                >
-                    <i className="bi bi-arrow-right-square-fill"></i>
-                </Button>
-            </div>
-        </div>
-    ) : (
-        <div>
-            <div>
-                <h5>.Informacion Personal</h5>
-                <p>Nombre Completo: Alejandro Bianchi</p>
-                <p>Email: alebianchi@gmail.com</p>
-                <p>Usuario: tubebitofiufiu</p>
-                <p>Foto de Perfil</p>
-                <h5>.Rol</h5>
-                <select name="role" id="role">
-                <option value="Admin">Administrador</option>
-                <option value="User">Usuario </option>
-                </select>
-                <h5>.Permisos</h5>
-                No tiene
-            </div>
-            <div className="d-flex justify-content-between">
-                <Button
-                    variant="primary"
-                    onClick={handleCloseModal}
-                    className='btn register btn-lg'
-                    style={{
-                        backgroundColor: window.themeColors.buttonColor,
-                        color: window.themeColors.footerColorText
-                    }}
-                >
-                    <i className="bi bi-arrow-left-square-fill"></i>
-                </Button>
-                <Button
-                    variant="primary"
-                    onClick={() => handleShowModal(1)}
-                    className='btn register btn-lg'
-                    style={{
-                        backgroundColor: window.themeColors.buttonColor,
-                        color: window.themeColors.footerColorText
-                    }}
-                >
-                    <i className="bi bi-arrow-right-square-fill"></i>
-                </Button>
-            </div>
-        </div>
-    );
-
     const [data, setData] = useState([
         ["Nombre y Apellido", "Información del Solicitante", "Fecha de Solicitud", "Fecha de Ingreso", "Dar de Alta /", "Dar Baja"],
-        ["Alejandro Bianchi", <ButtonFolder />, "29/08/23","29/09/19" , <ButtonCheck />, <ButtonClose />],
-        ["Fabricio Saucedo", <ButtonFolder />, "15/02/23","25/09/98" , <ButtonCheck />, <ButtonClose />],
-        ["Leandro Joel Ramos", <ButtonFolder />, "30/03/23","12/06/99" , <ButtonCheck />, <ButtonClose />],
+        ["Alejandro Bianchi", <ButtonFolder />, "29/08/23", "29/09/19", <ButtonCheck />, <ButtonClose />],
+        ["Fabricio Saucedo", <ButtonFolder />, "15/02/23", "25/09/98", <ButtonCheck />, <ButtonClose />],
+        ["Leandro Joel Ramos", <ButtonFolder />, "30/03/23", "12/06/99", <ButtonCheck />, <ButtonClose />],
     ]);
     const [filteredData, setFilteredData] = useState(data);
 
     const handleSearch = (query) => {
-      const filtro = query.toLowerCase().trim();
-      if (filtro === '') {
-        setFilteredData(data);
-      } else {
-        const resultados = data.filter((fila) =>
-          fila.some((item) => {
-            if (typeof item === 'string') {
-              return item.toLowerCase().includes(filtro);
-            }
-            return false;
-          })
-        );
-        setFilteredData(resultados);
-      }
+        const filtro = query.toLowerCase().trim();
+        if (filtro === '') {
+            setFilteredData(data);
+        } else {
+            const resultados = data.filter((fila) =>
+                fila.some((item) => {
+                    if (typeof item === 'string') {
+                        return item.toLowerCase().includes(filtro);
+                    }
+                    return false;
+                })
+            );
+            setFilteredData(resultados);
+        }
     };
     const modalTitle = currentModal === 1 ? "Informacion del Usuario" : "Informacion del Usuario";
 
@@ -162,15 +119,17 @@ function ListOfUsers() {
                         <h1 className='text-first d-flex justify-content-center'> Listado de Usuarios y Solicitantes </h1>
                     </div>
                     <div>
-                         <Searcher data={data} onSearch={handleSearch} />
+                        <Searcher data={data} onSearch={handleSearch} />
                     </div>
                     <div className='container-fluid table-container p-0 m-0' style={{ overflowX: 'auto', overflowY: 'auto', maxHeight: '25vw' }}>
                         <DataTable data={filteredData} headerBackgroundColor="#F2A57F" />
                         <CustomModal
-                            title={modalTitle}
-                            show={modalShow}
-                            handleClose={handleCloseModal}
+                            title="Título del Modal"
                             body={modalBody}
+                            show={showModal}
+                            handleClose={handleCloseModal}
+                            showNavButtons={true}
+                            onNavButtonClick={handleNavButtonClick}
                         />
                     </div>
                 </div>
