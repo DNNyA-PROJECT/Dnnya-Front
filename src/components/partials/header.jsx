bd6025c91f936758da5e444793ac96bbfc4d1dd3
Leandro joel Ramos <leandrojoelramos7@gmail.com>
2023-10-16 15:36:29 -0300

    mod : formSubmission, header

--- src/components/partials/header.jsx
+++ src/components/partials/header.jsx
@@ -7,55 +7,41 @@ function Header() {
   const defaultUser = {
     username: 'Usuario',
   };
-
+  
   const [selectedUser, setSelectedUser] = useState(defaultUser);
-
-  const fetchUser = async (userId) => {
+  
+  const fetchUser = async () => {
     try {
-      const response = await axios.get(`http://localhost:8080/Auth/profile${userId}`);
+      const response = await axios.get('http://localhost:8080/Auth/profile');
       const user = response.data;
-      setSelectedUser({ username: user.name });
+      setSelectedUser({ username: user.username });
     } catch (error) {
       console.error('Error fetching user:', error);
     }
   };
 
   useEffect(() => {
-
+    fetchUser();
   }, []);
 
   return (
-    <>
-      <div className='content-fluid d-flex flex-column flex-md-row align-items-center justify-content-around'>
-        <div className='text-center'>
+    <div className='content-fluid d-flex flex-column flex-md-row align-items-center justify-content-around'>
+      <div className='text-center'>
         <img src={DeFLogo} className='d-md-none DeFLogo' alt="img" />
-          <h5 className='text-first'>Sistema Informático de la Defensoria</h5>
-          <h5 className='text-second'>de los Derechos de Niños, Niñas y Adolescentes</h5>
-        </div>
+        <h5 className='text-first'>Sistema Informático de la Defensoria</h5>
+        <h5 className='text-second'>de los Derechos de Niños, Niñas y Adolescentes</h5>
+      </div>
 
-        <div className='container-fluid m-0 d-flex align-items-center justify-content-between d-md-none '>
-          <div className=' d-md-none'>
-            <button className="btn" style={{ backgroundColor: window.themeColors.buttonColor, color: window.themeColors.footerColorText }}><i className="bi bi-list"></i></button>
-          </div>
-          <div className=' d-md-none'>
-            <div className="dropdown">
-              <button className='user d-flex align-items-center dropdown-toggle' type="button" data-bs-toggle="dropdown" aria-expanded="false">
-                <img src={selectedUser.photo} alt="Usuario" className="user-photo" />
-                <div><h4>{selectedUser.name}</h4></div>
-              </button>
-              <ul className="dropdown-menu" style={{ backgroundColor: window.themeColors.footerBackground.bakgroundFColor }}>
-                <li><Link to="/Iniciar_sesion"><h3 className='text-link'>Iniciar Sesión</h3></Link></li>
-                <li><Link to="/Registro"><h3 className='text-link'>Registrarse</h3></Link></li>
-              </ul>
-            </div>
-          </div>
+      <div className='container-fluid m-0 d-flex align-items-center justify-content-between d-md-none '>
+        <div className=' d-md-none'>
+          <button className="btn" style={{ backgroundColor: window.themeColors.buttonColor, color: window.themeColors.footerColorText }}>
+            <i className="bi bi-list"></i>
+          </button>
         </div>
-
-        <div className='d-none d-md-block'>
+        <div className=' d-md-none'>
           <div className="dropdown">
             <button className='user d-flex align-items-center dropdown-toggle' type="button" data-bs-toggle="dropdown" aria-expanded="false">
-              <img src={selectedUser.photo} alt="Usuario" className="user-photo" />
-              <div><h4>{selectedUser.name}</h4></div>
+              <div><h4>{selectedUser.username}</h4></div>
             </button>
             <ul className="dropdown-menu" style={{ backgroundColor: window.themeColors.footerBackground.bakgroundFColor }}>
               <li><Link to="/Iniciar_sesion"><h3 className='text-link'>Iniciar Sesión</h3></Link></li>
@@ -64,8 +50,21 @@ function Header() {
           </div>
         </div>
       </div>
-    </>
+
+      <div className='d-none d-md-block'>
+        <div className="dropdown">
+          <button className='user d-flex align-items-center dropdown-toggle' type="button" data-bs-toggle="dropdown" aria-expanded="false">
+            <div><h4>{selectedUser.username}</h4></div>
+          </button>
+          <ul className="dropdown-menu" style={{ backgroundColor: window.themeColors.footerBackground.bakgroundFColor }}>
+            <li><Link to="/Iniciar_sesion"><h3 className='text-link'>Iniciar Sesión</h3></Link></li>
+            <li><Link to="/Registro"><h3 className='text-link'>Registrarse</h3></Link></li>
+          </ul>
+        </div>
+      </div>
+    </div>
   );
 }
 
 export default Header;
+
--- src/pages/formSubmission/formSubmission.jsx
+++ src/pages/formSubmission/formSubmission.jsx
@@ -1,13 +1,21 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import '../../assets/styles/styles.css'
-import '../../assets/styles/normalize.css'
-import Footer from '../../components/partials/footer.jsx'
-import Menu from '../../components/partials/Menu.jsx'
+import '../../assets/styles/styles.css';
+import '../../assets/styles/normalize.css';
+import Footer from '../../components/partials/footer.jsx';
+import Menu from '../../components/partials/Menu.jsx';
 
 function FormSubmission() {
     const [correoDestino, setEmail] = useState('');
+    const [isSubmitting, setIsSubmitting] = useState(false);
+    const [submissionResult, setSubmissionResult] = useState(null);
+
+    useEffect(() => {
+        if (submissionResult) {
+            console.log('Form submitted successfully:', submissionResult);
+        }
+    }, [submissionResult]);
 
     const handleEmailChange = (e) => {
         setEmail(e.target.value);
@@ -15,52 +23,70 @@ function FormSubmission() {
 
     const handleSubmit = async (e) => {
         e.preventDefault();
+        setIsSubmitting(true);
+
         try {
             const response = await axios.post('http://localhost:8080/enviarCorreo', {
-                correoDestino: correoDestino
+                correoDestino: correoDestino,
             });
-            console.log(response.data);
+            setSubmissionResult(response.data);
         } catch (error) {
-            console.error(error);
+            console.error('Error submitting form:', error);
+        } finally {
+            setIsSubmitting(false);
         }
     };
 
     return (
         <>
-            <div className='box container-fluid row p-0 ' style={{ backgroundColor: window.themeColors.footerBackground.bakgroundFColor }}>
-                <div className='col-md-2 m-0 container-fluid p-0 menubox d-none d-md-block' style={{ backgroundColor: window.themeColors.color }}>
+            <div className="box container-fluid row p-0" style={{ backgroundColor: window.themeColors.footerBackground.bakgroundFColor }}>
+                <div className="col-md-2 m-0 container-fluid p-0 menubox d-none d-md-block" style={{ backgroundColor: window.themeColors.color }}>
                     <Menu />
                 </div>
 
-                <div className='container-fluid d-flex align-items-center flex-column col-9 justify-content-between'>
-                    <div className='box-submission' style={{ backgroundColor: window.themeColors.color }}>
-                        <form onSubmit={handleSubmit} className=' d-flex flex-column align-items-center'>
-                            <h1 className='text-first'>Envio de Formulario de Registro</h1>
+                <div className="container-fluid d-flex align-items-center flex-column col-9 justify-content-between">
+                    <div className="box-submission" style={{ backgroundColor: window.themeColors.color }}>
+                        <form onSubmit={handleSubmit} className="d-flex flex-column align-items-center">
+                            <h1 className="text-first">Envío de Formulario de Registro</h1>
                             <input
                                 type="email"
                                 name="correoDestino"
                                 id="correoDestino"
                                 value={correoDestino}
                                 onChange={handleEmailChange}
-                                className='form-inputs form-control md'
-                                placeholder='Ingrese el Correo Electronico'
+                                className="form-inputs form-control md"
+                                placeholder="Ingrese el Correo Electrónico"
                             />
-                            <input
+                            <button
                                 type="submit"
-                                value="ENVIAR"
-                                className='btn register btn-lg mb-3'
-                                style={{ backgroundColor: window.themeColors.buttonColor, color: window.themeColors.footerColorText }}
-                            />
+                                className="btn register btn-lg mb-3"
+                                style={{
+                                    backgroundColor: window.themeColors.buttonColor,
+                                    color: window.themeColors.footerColorText,
+                                }}
+                                disabled={isSubmitting}
+                            >
+                                {isSubmitting ? 'Enviando...' : 'ENVIAR'}
+                            </button>
                         </form>
+                        {submissionResult && (
+                            <div className="submission-result">
+                                {submissionResult.success ? (
+                                    <p className="text-success">Formulario enviado exitosamente</p>
+                                ) : (
+                                    <p className="text-danger">Error al enviar el formulario: {submissionResult.error}</p>
+                                )}
+                            </div>
+                        )}
                     </div>
                 </div>
             </div>
 
-            <div className='container-fluid p-0 m-0'>
+            <div className="container-fluid p-0 m-0">
                 <Footer />
             </div>
         </>
-    )
+    );
 }
 
 export default FormSubmission;
