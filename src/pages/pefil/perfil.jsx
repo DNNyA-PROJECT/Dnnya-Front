cc5a97206009c34239ce192b8c5d1b337fc99072
Leandro joel Ramos <leandrojoelramos7@gmail.com>
2023-11-02 11:02:21 -0300

    mod : Perfil

--- src/pages/pefil/perfil.jsx
+++ src/pages/pefil/perfil.jsx
@@ -8,7 +8,7 @@ function perfil() {
             <div className=' container-fluid row p-0 m-0 ' style={{ backgroundColor: window.themeColors.footerBackground.bakgroundFColor }}>
 
                 <div className='container-fluid d-flex flex-column py-3 m-0 p-0'>
-                    <h3 className="fs-3 d-flex justify-content-center">Mi Perfil</h3>
+                    <h3 className="fs-3 d-flex justify-content-center">MI PERFIL</h3>
                     <form action="">
                         <div className="container py-5" style={{ backgroundColor: window.themeColors.footerBackground.bakgroundFColor }}>
                             <div className="d-flex flex-wrap py-5  container-fluid justify-content-center" style={{ backgroundColor: window.themeColors.boxColorBluSky }}>
@@ -21,7 +21,7 @@ function perfil() {
                                     <input type="text" name="" className="form-control md mb-3" id="" placeholder="Estado Civil" />
                                 </div>
                             </div>
-                            <h3 className="d-flex py-3 justify-content-center">Infromación de Contacto</h3>
+                            <h3 className="d-flex py-3 justify-content-center">INFORMACIÓN DE CONTACTO</h3>
                             <div className="d-flex flex-column container-fluid py-5" style={{ backgroundColor: window.themeColors.boxColorBluSky }}>
                                 <div className="flex-wrap  row d-flex justify-content-center">
                                     <div className="col-3">
@@ -35,7 +35,7 @@ function perfil() {
                                     <input type="email" name="" id="" className="form-control w-100 form-control md text-center" placeholder="Correo Electronico" />
                                 </div>
                             </div>
-                            <h3 className="d-flex py-3 justify-content-center">Contacto de Emergencia</h3>
+                            <h3 className="d-flex py-3 justify-content-center">CONTACTO DE EMERGENCIA</h3>
                             <div className="d-flex flex-column container-fluid py-5" style={{ backgroundColor: window.themeColors.boxColorBluSky }}>
                                 <div className="flex-wrap  row d-flex justify-content-center">
                                     <div className="col-3">
@@ -49,9 +49,9 @@ function perfil() {
                                     <input type="Text" name="" id="" className="form-control w-100 form-control md text-center" placeholder="Informacion medica" />
                                 </div>
                             </div>
-                            <h3 className="d-flex py-3 justify-content-center">Informacion Extra</h3>
+                            <h3 className="d-flex py-3 justify-content-center">INFORMACIÓN EXTRA</h3>
                             <div>
-                                <AccordionComponent buttonText={"Historial Educativo"} buttonClassName=" lightblue custom-btn mt-3 w-100 py-4 fs-4 m-0">
+                                <AccordionComponent buttonText={"Historial Educativo"} buttonClassName=" lightblue custom-btn w-100 py-4 fs-4 m-0">
                                     <div className="pb-5 d-flex justify-content-center" style={{ backgroundColor: window.themeColors.boxColorBluSky }}>
                                         <input type="text" name="" id="" className="form-control md w-50" placeholder="Títulos academicos, Instituciones eductativas, Fechas de graduación" />
                                     </div>
