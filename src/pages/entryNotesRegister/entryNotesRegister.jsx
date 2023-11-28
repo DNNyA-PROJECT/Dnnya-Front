0d1cc46fe68da5ebc9ed1ae35978cb7f3e2f0a89
Leandro joel Ramos <leandrojoelramos7@gmail.com>
2023-11-28 10:39:59 -0300

    mod : EntryNotesRegister

--- src/pages/entryNotesRegister/entryNotesRegister.jsx
+++ src/pages/entryNotesRegister/entryNotesRegister.jsx
@@ -1,6 +1,7 @@
 import Menu from '../../components/partials/Menu.jsx';
 import Header from '../../components/partials/header.jsx';
 import Footer from '../../components/partials/footer.jsx';
+import ComponentComment from '../../components/componentComment/componentComment.jsx';
 
 function entryNotesRegister() {
   return (
@@ -11,11 +12,50 @@ function entryNotesRegister() {
           <Menu />
         </div>
 
-        <div className='container-fluid d-flex align-items-center flex-column col-12 col-md-9 justify-content-between'>
+        <div className='container-fluid d-flex align-items-center flex-column col-12 col-md-9'>
           <div className='homeHead py-5 px-5 container-fluid'>
             <Header />
           </div>
+          <div className='container-fluid'>
+            <h4 className='fs-3 fw-bolder mb-5 d-flex justify-content-center'>
+              Registro notas de entrada
+            </h4>
+            <form action="">
+              <div className='row'>
+                <div className='d-flex justify-content-center'>
+                </div>
+                <div className='d-flex justify-content-evenly mb-3  row' >
+                  <input className='col-2  form-control md' type="number" name="" id="" placeholder='Número de la Nota de Entrada' />
+                  <input
+                    className='col-2 form-control md'
+                    type="date"
+                    id="fecha"
+                  />
+                </div>
+                <div className='d-flex justify-content-evenly  row' >
+                  <input className='col-2 form-control md' type="text" name="" id="" placeholder='Responsable del Registro' />
+                  <input className='col-2 form-control md' type="text" name="" id="" placeholder='Responsable del Seguimiento' />
+                </div>
+              </div>
+
+              <div className='d-flex mx-5 my-3 justify-content-center'>
+                <div className='col-10'>
+                  <select className='form-control' name="" id="">
+                    <option value="Pendiente">pendiente</option>
+                    <option value="Aprobado">Aprobado</option>
+                    <option value="Rechazado">Rechazado</option>
+                    <option value="Cerrado">Cerrado</option>
+                  </select>
+                </div>
+              </div>
+              <ComponentComment titulo="Nota de Entrada Comentario" placeholder="Escribe aquí tus comentarios" />
+              <div className='d-flex justify-content-center my-3'>
+                <button className='btn fs-4 fw-bolder' type='submit' style={{ backgroundColor: window.themeColors.buttonColor, color: window.themeColors.footerColorText }}>ENVIAR</button>
+              </div>
+            </form>
+          </div>
         </div>
+
       </div>
 
 
